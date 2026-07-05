9330296bd46430677ed65091d31265e0562d9adc
wrobeloliver2-crypto <wrobeloliver2@gmail.com>
2026-07-05 14:28:15 +0200

    Rebuild-Trigger fuer Like/Kommentar/Termin-Feature

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// build: 1783254019
+// build: 1783254495
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173, host: true },
