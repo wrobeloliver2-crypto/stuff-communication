552fc27eea7a13fe2a51f6c804cff4d538cea6a8
wrobeloliver2-crypto <wrobeloliver2@gmail.com>
2026-07-05 13:34:04 +0200

    Rebuild-Trigger fuer Datenverlust-Schutz

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// build: 1783249285
+// build: 1783251244
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+  server: { port: 5173, host: true },
+  build: { outDir: 'dist', sourcemap: false },
 })
